661430ec2931146eda5aef5ad32094d032eb84c2
GueroehGB <goru.Akiba@gmail.com>
2020-11-08 08:52:54 +0000

    add .env_example

--- model/connectToDb.js
+++ model/connectToDb.js
@@ -3,10 +3,11 @@ const mongoose = require ("mongoose");
 // database
 const dbname = "hook-rss";
 if(process.env.NODE_ENV === "production"){
-    const dbuser = process.env.DB_User,
-        dbpassword = process.env.DB_Password;
+    const dbuser = process.env.dbuser,
+        dbpassword = process.env.dbpassword;
         dbhost = process.env.dbhost;
     var url_server = `mongodb+srv://${dbuser}:${dbpassword}@${dbhost}/${dbname}?retryWrites=true&w=majority`;
+		// console.log(url_server)
 
 } else {
     var url_server = `mongodb://localhost:27017/${dbname}`;
